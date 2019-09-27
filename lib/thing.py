8d1bd7c8351767605ea1dc89f508132150e8b1ea
Ben Robertson <bdrobertson.br@gmail.com>
2019-09-27 18:11:20 -0400

    Render random heat map

--- lib/thing.py
+++ lib/thing.py
@@ -1,6 +1,7 @@
-import csv
 import pandas as pd
-
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def run():
     columns = ['Zip Code', 'Month', '2018', '2017']
@@ -8,5 +9,12 @@ def run():
     dataframe = dataframe[columns]
     dataframe = dataframe.melt(id_vars=['Zip Code', 'Month'], var_name ='Year', value_name = 'CE_Sales')
 
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     print(dataframe)
+
+    Index = ['aaa', 'bbb', 'ccc', 'ddd', 'eee']
+    Cols = ['A', 'B', 'C', 'D']
+    df = pd.DataFrame(abs(np.random.randn(5, 4)), index=Index, columns=Cols)
+
+    sns.heatmap(df, annot=True)
+    plt.savefig('images/heatmap.png')

d6a2b4012b2952d64b448debd5b89ee4de206ece
Ben Robertson <bdrobertson.br@gmail.com>
2019-09-27 18:40:59 -0400

    Try and fail to make heatmap of dataframe

--- lib/thing.py
+++ lib/thing.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -8,13 +7,18 @@ def run():
     dataframe = pd.read_csv('csvs/data.csv')
     dataframe = dataframe[columns]
     dataframe = dataframe.melt(id_vars=['Zip Code', 'Month'], var_name ='Year', value_name = 'CE_Sales')
+    zip_codes = dataframe['Zip Code'].unique()
 
-    # import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
     print(dataframe)
 
-    Index = ['aaa', 'bbb', 'ccc', 'ddd', 'eee']
-    Cols = ['A', 'B', 'C', 'D']
-    df = pd.DataFrame(abs(np.random.randn(5, 4)), index=Index, columns=Cols)
+    sns.set_context("talk")
+    f, ax = plt.subplots(figsize=(11, 15))
+    #
+    # ax = sns.heatmap(dataframe, annot=True, fmt="d", linewidths=.5, ax=ax, xticklabels=['Jan', 'Feb', 'Mar'],
+    #                  yticklabels=zip_codes)
+    ax = sns.heatmap(dataframe)
+    ax.axes.set_title("Heatmap of Ride Counts by Day and Hour of Day", fontsize=24, y=1.01)
+    ax.set(xlabel='Day of Week', ylabel='Starting Hour of Ride');
 
-    sns.heatmap(df, annot=True)
     plt.savefig('images/heatmap.png')

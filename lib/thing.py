771062ff4dd991b761e8c3375ca9794620867f08
Ben Robertson <bdrobertson.br@gmail.com>
2019-09-27 17:52:07 -0400

    melt two year columns into rows

--- lib/thing.py
+++ lib/thing.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 
 def run():
+    columns = ['Zip Code', 'Month', '2018', '2017']
     dataframe = pd.read_csv('csvs/data.csv')
+    dataframe = dataframe[columns]
+    dataframe = dataframe.melt(id_vars=['Zip Code', 'Month'], var_name ='Year', value_name = 'CE_Sales')
+
     import pdb; pdb.set_trace()
     print(dataframe)

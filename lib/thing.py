f5ab2fa347edb24d14851f332f7d58129eba323b
Ben Robertson <bdrobertson.br@gmail.com>
2019-09-28 11:39:33 -0400

    Get 2017 data into heatmap image
    Very early and basic heatmap so far.
    based on the seaborn docs here:
    https://seaborn.pydata.org/generated/seaborn.heatmap.html

--- lib/thing.py
+++ lib/thing.py
@@ -6,19 +6,21 @@ def run():
     columns = ['Zip Code', 'Month', '2018', '2017']
     dataframe = pd.read_csv('csvs/data.csv')
     dataframe = dataframe[columns]
-    dataframe = dataframe.melt(id_vars=['Zip Code', 'Month'], var_name ='Year', value_name = 'CE_Sales')
-    zip_codes = dataframe['Zip Code'].unique()
+    pivot_2017 = dataframe.pivot('Zip Code', 'Month', '2017')
+    sns.heatmap(pivot_2017)
+    # dataframe = dataframe.melt(id_vars=['Zip Code', 'Month'], var_name ='Year', value_name = 'CE_Sales')
+    # zip_codes = dataframe['Zip Code'].unique()
 
-    import pdb; pdb.set_trace()
-    print(dataframe)
+    # import pdb; pdb.set_trace()
+    # print(dataframe)
 
-    sns.set_context("talk")
-    f, ax = plt.subplots(figsize=(11, 15))
-    #
-    # ax = sns.heatmap(dataframe, annot=True, fmt="d", linewidths=.5, ax=ax, xticklabels=['Jan', 'Feb', 'Mar'],
-    #                  yticklabels=zip_codes)
-    ax = sns.heatmap(dataframe)
-    ax.axes.set_title("Heatmap of Ride Counts by Day and Hour of Day", fontsize=24, y=1.01)
-    ax.set(xlabel='Day of Week', ylabel='Starting Hour of Ride');
+    # sns.set_context("talk")
+    # f, ax = plt.subplots(figsize=(11, 15))
+    # #
+    # # ax = sns.heatmap(dataframe, annot=True, fmt="d", linewidths=.5, ax=ax, xticklabels=['Jan', 'Feb', 'Mar'],
+    # #                  yticklabels=zip_codes)
+    # ax = sns.heatmap(dataframe)
+    # ax.axes.set_title("Heatmap of Ride Counts by Day and Hour of Day", fontsize=24, y=1.01)
+    # ax.set(xlabel='Day of Week', ylabel='Starting Hour of Ride');
 
-    plt.savefig('images/heatmap.png')
+    plt.savefig('images/2017_heatmap.png')

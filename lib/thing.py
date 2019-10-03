1b3fcde287086807408624b0fbcbd8c0630a93bc
Ben Robertson <bdrobertson.br@gmail.com>
2019-10-02 23:31:02 -0400

    fixed formatting and cell values

--- lib/thing.py
+++ lib/thing.py
@@ -6,10 +6,15 @@ def run():
     columns = ['Zip Code', 'Month', '2018', '2017']
     dataframe = pd.read_csv('csvs/data.csv')
     dataframe = dataframe[columns]
+    dataframe = dataframe.fillna(0)
     # pivot_2017 = dataframe.pivot('Zip Code', 'Month', '2017')
     pivot_2018 = dataframe.pivot('Zip Code', 'Month', '2018')
-    f, ax = plt.subplots(figsize=(11, 15))
-    sns.heatmap(pivot_2018, ax = ax)
+    pivot_2018 = pivot_2018.fillna(1).astype(int)
+    # pivot_2017 = pivot_2017.round()
+
+    f, ax = plt.subplots(figsize=(15, 20))
+    # ax = sns.heatmap(pivot_2017)
+    sns.heatmap(pivot_2018, fmt='g', robust=True, annot=True, ax=ax, cmap="Reds", linewidths=.3, xticklabels=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
     # dataframe = dataframe.melt(id_vars=['Zip Code', 'Month'], var_name ='Year', value_name = 'CE_Sales')
     # zip_codes = dataframe['Zip Code'].unique()
 
@@ -23,6 +28,7 @@ def run():
     # #                  yticklabels=zip_codes)
     # ax = sns.heatmap(dataframe)
     # ax.axes.set_title("Heatmap of Ride Counts by Day and Hour of Day", fontsize=24, y=1.01)
-    # ax.set(xlabel='Day of Week', ylabel='Starting Hour of Ride');
+    # ax.set(xlabel='Day of Week', ylabel='Star
+    # ting Hour of Ride');
 
     plt.savefig('images/2018_heatmap.png')
